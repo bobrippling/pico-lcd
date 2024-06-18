5a9f1cb633e8091523e02b3d7339654a31a1452d
Rob Pilling <robpilling@gmail.com>
2024-06-18 07:40:52 +0100

    Use GC9A01A LCD driver

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,4 @@
-/*
-#![no_std]
-#![no_main]
-
-use defmt_rtt as _;
-use embassy_executor::Spawner;
-use embassy_futures::join::join;
-use embassy_rp::{
-    bind_interrupts,
-};
-use embassy_sync::blocking_mutex::raw::NoopRawMutex;
-use panic_probe as _;
-
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_rp::init(Default::default());
-
-    waveshare_rp2040_lcd_0_96::pac::t
-
-    panic!()
-}
-*/
-//! Example of graphics on the LCD of the Waveshare RP2040-LCD-0.96
+//! Example of graphics on the LCD of the Waveshare RP2040-LCD-1.28-Round.
 //!
 //! Draws a red and green line with a blue rectangle.
 //! After that it fills the screen line for line, at the end it starts over with
@@ -33,7 +11,7 @@ use embedded_graphics::primitives::Line;
 use fugit::RateExtU32;
 use panic_halt as _;
 
-use waveshare_rp2040_lcd_0_96::entry;
+use waveshare_rp2040_lcd_0_96::entry; // TODO: get 1_28_round BSP
 use waveshare_rp2040_lcd_0_96::{
     hal::{
         self,
@@ -51,7 +29,9 @@ use embedded_graphics::{
     prelude::*,
     primitives::{PrimitiveStyle, PrimitiveStyleBuilder, Rectangle},
 };
-use st7735_lcd::{Orientation, ST7735};
+
+use display_interface_spi::SPIInterface;
+use gc9a01a::GC9A01A;
 
 const LCD_WIDTH: u32 = 240;
 const LCD_HEIGHT: u32 = 240;
@@ -89,17 +69,28 @@ fn main() -> ! {
 
     let (mut _pio, _sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
 
-    // https://www.waveshare.com/wiki/RP2040-LCD-0.96
-    // ST7735S LCD
     let lcd_dc = pins.gp8.into_push_pull_output();
+    let lcd_cs = pins.gp9.into_push_pull_output();
     let lcd_clk = pins.gp10.into_function::<hal::gpio::FunctionSpi>();
     let lcd_mosi = pins.gp11.into_function::<hal::gpio::FunctionSpi>();
     let lcd_rst = pins
-        .gp12 // FIXME: 13?
+        .gp12
         .into_push_pull_output_in_state(hal::gpio::PinState::High);
-    let mut _lcd_bl = pins
-        .gp25
-        .into_push_pull_output_in_state(hal::gpio::PinState::High);
+
+    let lcd_bl = {
+        // Init PWMs
+        let pwm_slices = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
+
+        // Configure PWM4
+        let mut pwm = pwm_slices.pwm4;
+        pwm.set_ph_correct();
+        pwm.enable();
+
+        // Output channel B on PWM4 to the backlight pin
+        let mut lcd_bl = pwm.channel_b;
+        lcd_bl.output_to(pins.gp25);
+        lcd_bl
+    };
 
     let spi = hal::Spi::<_, _, _, 8>::new(pac.SPI1, (lcd_mosi, lcd_clk));
 
@@ -111,16 +102,13 @@ fn main() -> ! {
         embedded_hal::spi::MODE_0,
     );
 
-    // LCD is a 65K IPS LCD 160x80, color order is BGR and a offset 1,26 pixel.
-    // LCD controller can correct this by settings the order bit (bit 3) in MADCTL register.
-    // Also the colours are inverted, LCD controller can also correct this by writing to INVON register with no paramters.
-    // All this is handled by the ST7735 crate.
-    let mut display = ST7735::new(spi, lcd_dc, lcd_rst, false, true, LCD_WIDTH, LCD_HEIGHT);
+    let spi_interface = SPIInterface::new(spi, lcd_dc, lcd_cs);
 
-    display.init(&mut delay).unwrap();
-    display.set_orientation(&Orientation::Landscape).unwrap();
+    let mut display = GC9A01A::new(spi_interface, lcd_rst, lcd_bl);
 
-    display.set_offset(1, 26);
+    display.initialize(&mut delay).unwrap();
+
+    display.set_backlight(u16::MAX / 2);
 
     let lcd_zero = Point::zero();
     let lcd_max_corner = Point::new((LCD_WIDTH - 1) as i32, (LCD_HEIGHT - 1) as i32);
